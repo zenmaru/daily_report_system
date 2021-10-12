f7b1fc897a176ad22c8ef41bf881e4e82ed89d95
自分の名前 <>
2021-10-13 01:10:43 +0900

    従業員destroyアクションの作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -195,4 +195,20 @@ public class EmployeeAction extends ActionBase {
             }
         }
     }
+    /*
+     * 倫理削除を行う
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void destroy() throws ServletException,IOException{
+        //CSRF対策 tokenのチェック
+        if(checkToken()) {
+            //idを条件に従業員データを倫理削除する
+            service.destroy(toNumber(getRequestParam(AttributeConst.EMP_ID)));
+            //セッションに削除完了のフラッシュメッセージを設定
+            putSessionScope(AttributeConst.FLUSH,MessageConst.I_DELETED.getMessage());
+            //一覧画面にリダイレクト
+            redirect(ForwardConst.ACT_EMP,ForwardConst.CMD_INDEX);
+        }
+    }
 }
